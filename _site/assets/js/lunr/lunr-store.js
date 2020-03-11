df2cd2c3117c1e6752f6f3e7c6032012773fb764
hsierra37 <hernando.sierra@tecwis.com>
2020-03-11 19:40:49 -0400

    restore _config.yml file

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,9 +3,11 @@ var store = [{
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "http://0.0.0.0:4000/services/cloud_computing/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Web Development",
         "excerpt":"Here is a sample service collection   I can describe the service here !!!!  ","categories": [],
         "tags": [],
         "url": "http://0.0.0.0:4000/services/development/",
-        "teaser":null}]
+        "teaser": null
+      }]
